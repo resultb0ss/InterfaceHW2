57ba863e08f53161a3504e27d9d062ff5afeb69e
Admin <reteamboss@gmail.com>
2024-08-29 16:49:37 +0300

    Задание 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+    var numOne = 10
+    var numTwo = 340
+    while(numTwo != 0){
+        val num = numTwo
+        numTwo = numOne % numTwo
+        numOne = num
+    }
+    println(numOne)
 }
